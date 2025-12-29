78a10efb375ca952cad31608347bedebe403b6e0
Shobi <s.olaadisa@alustudent.com>
2025-12-29 16:15:49 +0200

    remove; discards /dist from version control

--- dist/agents_starter/index.js
+++
@@ -1,9 +0,0 @@
-import { C, w } from "./assets/worker-entry--dhXs5ys.js";
-import "node:events";
-import "node:stream";
-import "node:async_hooks";
-import "cloudflare:workers";
-export {
-  C as Chat,
-  w as default
-};
--- dist/client/assets/clone-C_TPxZGe.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-Cw12-qEW.js";var e=4;function a(o){return r(o,e)}export{a as c};

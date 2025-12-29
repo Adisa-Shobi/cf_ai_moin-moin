e93bc82694fe535b3abfd18325ea1a76b6b44d2d
Shobi <s.olaadisa@alustudent.com>
2025-12-29 16:11:25 +0200

    update: changes model used for agent operations

---
+++ dist/agents_starter/index.js
@@ -0,0 +1,9 @@
+import { C, w } from "./assets/worker-entry--dhXs5ys.js";
+import "node:events";
+import "node:stream";
+import "node:async_hooks";
+import "cloudflare:workers";
+export {
+  C as Chat,
+  w as default
+};
---
+++ dist/client/assets/clone-C_TPxZGe.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-Cw12-qEW.js";var e=4;function a(o){return r(o,e)}export{a as c};
